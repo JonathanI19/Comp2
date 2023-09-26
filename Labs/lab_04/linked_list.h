e22d3e21881cb0e398d636f25c512c06c274ab14
JonathanI19 <jmisely19@gmail.com>
2023-09-25 22:11:05 -0400

    Lab_04 initial setup

---
+++ Labs/lab_04/linked_list.h
@@ -0,0 +1,79 @@
+#ifndef _linked_list_h_
+#define _linked_list_h_
+
+namespace ll_node{
+    // node for the linked list data
+    // do not edit
+    class node{
+        
+        private:
+        int data;
+        node* next;
+
+        public:
+        
+        // constructor
+        node(int d){
+            data = d;
+            next = nullptr;
+        };
+
+        // getter, data
+        int get_data(){
+            return data;
+        }
+
+        // setter, 'next' pointer
+        void set_next(node *n){
+            next = n;
+        }
+
+        // getter, 'next' pointer
+        node* get_next(){
+            return next;
+        }
+    };
+}
+
+class linked_list{
+
+    // private class variables
+    private:
+
+    ll_node::node* head;
+    int n_elements;
+
+    // public class methods
+    public:
+
+    linked_list(){
+        head = nullptr;
+        n_elements = 0;
+    }
+
+    void insert(int d){
+        ll_node::node *newNode = new ll_node::node(d);
+        newNode->set_next(head);
+        head = newNode;
+        n_elements++;
+    }
+
+    int pop(){
+        int ret_val = 0;
+        if (n_elements > 0){
+            ret_val = head->get_data();
+            ll_node::node *tmpNode = head;
+            head = head->get_next();
+            delete tmpNode;
+            n_elements --;
+        }
+        return ret_val;
+    }
+
+    void clear_list(){}
+    bool is_empty(){}
+    int size(){}
+    int sum(){}
+};
+
+#endif
