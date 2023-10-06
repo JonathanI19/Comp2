0ec26fd318507129117cfd2128420e423b40739d
JonathanI19 <jmisely19@gmail.com>
2023-10-06 18:59:34 -0400

    Magical Debugging

--- Projects/project_2/proj_2.cpp
+++ Projects/project_2/proj_2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include "linked_list.h"
+#include<vector>
 using namespace std;
 
 #define DEFAULT_DIRECTORY "/data/courses/ece_3822/current/project_2/"
@@ -25,7 +26,26 @@ class linked_list_movies : public linked_list<int>{
     public:
 
     void display(){
-        // WRITE CODE TO DISPLAY THE LIST OF MOVIES
+        node* curr_ptr = head;
+        while (curr_ptr != nullptr) {
+            cout <<" " << curr_ptr->data;
+            curr_ptr = curr_ptr->next;
+        }
+        return;
+    }
+
+    bool has_watched(int target_movie) {
+        cout << endl << "entered has watched" << endl;
+        bool watched = false;
+        node* curr_ptr = head;
+        while (curr_ptr != nullptr) {
+            cout << endl << "Entered has watched while loop" << endl;
+            if (curr_ptr->data == target_movie) {
+                watched = true;
+            }
+            curr_ptr = curr_ptr->next; // FUCK THIS LINE IN PARTICULAR
+        }
+        return watched;
     }
 };
 
@@ -35,8 +55,19 @@ class customer{
     int customer_id;
     linked_list_movies movie_ids;    
 
+    // Constructor
+    customer(int id, vector<int> arr){
+        customer_id = id;
+        for(int i = 0; i < arr.size(); i++){
+            movie_ids.insert(arr.at(i));
+        }
+    }
+
     void display(){
-        // WRITE CODE TO DISPLAY THE CUSTOMER AND MOVIES
+        cout << "Customer " << customer_id << " watched";
+        movie_ids.display();
+        cout << endl;
+        return;
     }
 
 };
@@ -49,29 +80,84 @@ class linked_list_customers : public linked_list<customer*>{
 
     public:
 
-    void print_customer(int customer_id){
-        // WRITE THIS FUNCTION
+    void print_customer(int target_id){
+        node* curr_ptr = head;
+        while (curr_ptr != nullptr) {
+            if (curr_ptr->data->customer_id == target_id) {
+                curr_ptr->data->display();
+            }
+            curr_ptr = curr_ptr->next;
+        }
+
     }
 
     void print_all(){
-        // WRITE THIS FUNCTION
+        node* curr_ptr = head;
+        while (curr_ptr != nullptr) {
+            curr_ptr->data->display();
+
+            curr_ptr = curr_ptr->next;
+        }
     }
 
-    void print_views(int movie_id){
-        // WRITE THIS FUNCTION
+    void print_views(int target_id){
+        int count = 0;
+        node* curr_ptr = head;
+        while (curr_ptr != nullptr) {
+            cout << endl << "within print views while loop" << endl;
+            if (curr_ptr->data->movie_ids.has_watched(target_id)) {
+                cout << endl << "within print views if statement" << endl;
+                count ++;
+            }
+            curr_ptr = curr_ptr->next;
+            cout << endl << "Bottom of print_views while loop" << endl;
+        } 
+
+        cout << "Movie " << target_id << " has been watched " << count << "time(s)." << endl;      
+
     }
 
     void load_customers(string filename){
 
-        /*
-        This code might be useful:
+        
+        //This code might be useful:
         ifstream    file_id(filename);
-        string line
+        string line;
         int value;
-        while (getline(file_id,line))
-            value = stoi(line)          
-        */
+        bool cust_id = true;
+        bool movie_count = false;
+        int count = 0;
+        int i = 0;
+        int c_id;
+        vector<int> movies;
 
+        while (getline(file_id,line)){
+            
+            value = stoi(line);          
+            if (cust_id == true) {
+                c_id = value;
+                cust_id = false;
+                movie_count = true;
+            }
+            else if (movie_count == true) {
+                count = value;
+                movie_count = false;
+            }
+            else if (i < count) {
+                movies.push_back(value);
+                i++;    
+                
+                if (i == count) {
+                    customer* temp = new customer(c_id, movies);
+                    this->insert(temp);
+                    cust_id = true;
+                    movies.clear();
+                    i = 0;
+                }
+            }
+
+
+        }
     }
 
 };
@@ -84,9 +170,11 @@ int main(int argc, char** argv){
 
     linked_list_customers clients;
     clients.load_customers(filename);
+    
 
     clients.print_all();
     clients.print_customer(724);
+    cout << endl << "between customer and view" << endl;
     clients.print_views(109); 
 
     return 0;
