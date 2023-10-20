0ac377a2ab8a9c920d0c720e6f3e9f02cbaeaced
JonathanI19 <jmisely19@gmail.com>
2023-10-19 21:16:32 -0400

    Collision avoidance for insert and lookup

--- Labs/lab_06/lab_06.py
+++ Labs/lab_06/lab_06.py
@@ -24,6 +24,14 @@ class student:
 
     def display(self):
         print(self.last, " ", self.first , " " , self.gpa , " " , self.zip)
+        
+    # Getter for last name
+    def get_last(self):
+        return self.last
+
+    # Getter for first name
+    def get_first(self):
+        return self.first
 
 #################################################################
 class hash_table:
@@ -74,7 +82,24 @@ class hash_table:
             
         # Executes if index is not empty
         else:
-            print("INDEX: ", index, " Is currently occupied.")
+            start = index
+            
+            # Runs until break condition is met
+            while(True):
+                
+                # Increments index in circular fashion
+                index = ((index + 1) % self.buff_len)
+                
+                # If empty location found, insert student and then break
+                if (self.buffer[index] is None):
+                    self.buffer[index] = std
+                    break
+                
+                # Breaks out of loop if it reaches the start point without having found open space
+                elif(index == start):
+                    print("No available space left")
+                    break
+                
 
     # look up a student in the hash table by, 'student' or 'first' and 'last'
     def lookup(self,last,first,display=False):
@@ -85,7 +110,26 @@ class hash_table:
         
         # If location of index is populated in buffer, set target_student to equal that object
         if (self.buffer[index] is not None):
-            target_student=self.buffer[index]
+            start = index
+            while(True):
+                
+                # Break out of loop if empty space found before proper student
+                if ((start != index) and (self.buffer[index] is None)):
+                    break
+                
+                # Set target_student and break out of loop if proper location is found
+                elif((self.buffer[index].get_last().upper() == last.upper()) and ((self.buffer[index].get_first().upper() == first.upper()))):
+                    target_student = self.buffer[index]
+                    break
+                
+                # Increment index if strings don't match
+                else:
+                    index = ((index + 1) % self.buff_len)
+                
+                # if loop finds way back to initial posession, break  
+                if (start == index):
+                    break
+                
 
         # display results if user requests it
         if display: 
@@ -113,17 +157,18 @@ def main():
 
     # create the hash table
     # A larger buff_len means less chance of coincidental collisions
-    my_table = hash_table(buff_len = 30)
+    my_table = hash_table(buff_len = 6)
 
     # create two new students and insert them into the hash table
     my_table.insert( student( "Obeid" , "Iyad" , 19143 , 3.99) )
     my_table.insert( student( "Obeid" , "Timmy", 19147 , 3.14) )
-    my_table.insert( student( "OBEID", "TIMMwY", 19147 , 3.14))
+    my_table.insert( student( "OBEID", "IYAD", 19147 , 3.14))
     my_table.insert( student( "diebO" , "Timmy", 19147 , 3.14) )
+    my_table.insert( student( "diebO" , "TIMMY", 19147 , 3.14) )
     # look up a student and display results
     my_table.lookup("Obeid","Iyad",display=True)
     my_table.lookup("bob", "thebuilder", display = True)
-
+    my_table.lookup("diebo", "timMy", display = True)
     # display contents of hash table
     my_table.display()
 
