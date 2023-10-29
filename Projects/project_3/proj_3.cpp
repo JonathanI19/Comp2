eb644d485094770df73b3f784f40f895e1f71ebc
JonathanI19 <jmisely19@gmail.com>
2023-10-29 15:13:20 -0400

    Tasks 1-4 tentatively completed

--- Projects/project_3/proj_3.cpp
+++ Projects/project_3/proj_3.cpp
@@ -1,8 +1,8 @@
 /* PROJECT 3 - proj_3.cpp
 
-    @author
-    @contact
-    @date
+    @author Jonathan Isely
+    @contact jmisely19@gmail.com
+    @date 10/29/2023
 
     Implements a system that models reading data asynchronously from the cloud,
     storing it temporarily in a buffer, and then placing it into a hash table.
@@ -20,20 +20,148 @@
 #include<thread>
 #include<fstream>
 #include<vector>
+#include<deque>
 #include<chrono>
 #include<mutex>
-
+#include "math.h"
 using namespace std;
 using namespace std::chrono;
 
-// YOUR CODE HERE
-struct student{};
+
+/// @brief Student Struct
+struct student{
+
+    string first_name, last_name, major;
+    float gpa;
+    int ID;
+};
 
 // YOUR CODE HERE
-class hashTable{};
+class hashTable{
+    private:
+    int num_elements;
 
-// YOUR CODE HERE
-class queue{};
+    // 51971 is a prime number that we can use as a length for our hash_function. This is a temporary band-aid.
+    // Ideally, each member in the hash table would be a linked list node so we can start
+    // off much smaller and dynamically handle collisions. However, for the sake of brevity
+    // I will have to hold off on implementing that.
+    const static int buff_len = 51971;
+    student* buffer[buff_len];
+
+    /// @brief Generates hash key based off of multiplication method of hashing
+    /// @param id (int) ID from relevant student object
+    /// @return (int) Computed Hash Key
+    int hash_function(int id){
+
+        // Fractional value between 0 and 1
+        float c = 0.458762346;
+
+        // Get fractional part of id*c
+        float k = (float)id*c;
+        k = k-(long)k;
+
+        // Multiply buff_len by fractional part and round down.
+        int val = floor((float)buff_len * k);
+        return val; 
+    }
+
+
+    void collision_handler(student* s, int key){
+        int start = key;
+
+        if (buffer[key] == nullptr){
+            buffer[key] = s;
+        }
+        else {
+            while(buffer[key] != nullptr){
+                key = key + 1 % buff_len;
+                if (start == key) {
+                    cout << "Hash table full" << endl;
+                    return;
+                }
+            }
+            buffer[key] = s;
+        }
+    }
+    
+    public:
+
+    /// @brief Constructor
+    hashTable(){
+        num_elements = 0;
+        
+        // Setting each value of buffer to nullptr
+        for (int i = 0; i < buff_len; i++){
+            buffer[i] = nullptr;
+        }
+    }
+
+    void insert(student* s){
+        
+        // Error handling for if hash table is full
+        if (num_elements == buff_len) {
+            cout << "Hash Table Full" << endl;
+            return;
+        }
+
+
+        int key = hash_function(s->ID);
+
+        // Insert w/Collision handling
+        collision_handler(s, key);
+
+
+    }
+
+
+
+};
+
+/// @brief Class used to store incoming data in queue to be popped off as needed
+class queue{
+
+    private:
+    // Variable declarations
+    deque<student*> student_queue;
+    int num_elements;
+    bool isEmpty;
+    student* temp;
+
+    public:
+    /// @brief Constructor
+    queue(){
+        num_elements = 0;
+        bool isEmpty = true;
+    }
+
+    /// @brief Insert pointer to student object into queue
+    /// @param t (student*) Pointer to student object
+    void insert(student* t){
+        student_queue.push_back(t);
+        num_elements++;
+        isEmpty = false;
+    }
+
+    /// @brief Pop student pointer off front of queue
+    /// @return (student*) Pointer to student object
+    student* pop(){
+
+        temp = student_queue.front();
+        student_queue.pop_front();
+        num_elements--;
+        if (num_elements == 0) {
+            isEmpty = true;
+        }
+        return temp;
+    }
+
+    /// @brief Checks to see if queue is empty
+    /// @return (bool) True if empty; False if populated with data
+    bool is_empty(){
+        return isEmpty;
+    }
+
+};
 
 // You will need these global variables
 queue waiting_list;
@@ -93,13 +221,23 @@ void load_data(string filename){
             ;
         }
 
-        // YOUR CODE HERE
-        // [1] create a new student
+        // Create new student object from struct
+        student temp_student;
+        temp_student.first_name = first;
+        temp_student.last_name = last;
+        temp_student.gpa = gpa;
+        temp_student.ID = id;
+        temp_student.major = major;
+        
+        // Generate pointer to student and allocate on heap
+        student *p_student = &temp_student;
+        new(p_student) student;
+
 
         queue_mutex.lock();
 
         // YOUR CODE HERE
-        // [2] add the new student to the wait queue
+        waiting_list.insert(p_student);
 
         queue_mutex.unlock();
 
