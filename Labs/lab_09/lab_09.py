d0912d5d14102611dc9145b31046426a3a78009e
JonathanI19 <jmisely19@gmail.com>
2023-12-03 19:16:10 -0500

    AStar now slightly more efficient than Djikstra's

--- Labs/lab_09/lab_09.py
+++ Labs/lab_09/lab_09.py
@@ -1,12 +1,13 @@
 import heapq
 from argparse import ArgumentParser
+from math import sqrt
 
 ''' lab_09.py
 
-    YOUR NAME HERE
-    YOUR EMAIL ADDRESS
-    DATE
-
+    Jonathan Isely
+    jonathan.isely@temple.edu
+    Due 12/3/2023
+    
     Description:
     Implement the A* algorithm for graph path search. 
     Full instructions in readme.txt       
@@ -97,8 +98,10 @@ def djk(grid , loc_start , loc_end):
 
     h = []
 
+    count = 0
+    
     while loc_curr != loc_end:
-
+        count+=1
         incomplete_keys = d_incomplete.keys()
 
         # update all 4 neighbors of current node
@@ -136,6 +139,110 @@ def djk(grid , loc_start , loc_end):
         # to complete dictionary
         d_complete[loc_next] = d_incomplete[loc_next]
         del d_incomplete[loc_next]
+
+
+    print("Num of loops: ", count)
+    # return shortest path length
+    return d_complete[loc_end][0]
+
+def AStar(grid , loc_start , loc_end):
+    '''Find shortest path from loc_start to loc_end in grid
+    following the rules spelled out in AoC 2022 / Day 12
+    
+    Args:
+        grid:       list of lists containing grid values
+        loc_start:  tuple of i,j coordinates of start location
+        loc_end:    tuple of i,j coordinates of end location
+    Return:
+        n:          length of shortest path
+    '''
+
+    # init misc vars
+    n_rows,n_cols = len(grid) , len(grid[0])
+    INF = int(1e6)
+
+    # init dictionaries
+    d_incomplete,d_complete = {} ,{}
+
+    # add all nodes to the <incomplete> dictionary
+    for i in range(n_rows):
+        for j in range(n_cols):
+            d_incomplete[(i,j)] = (INF,(99,99))
+
+    # move <start> to <complete> list with distance 0
+    d_complete[loc_start] = (0 , (99,99))
+    del d_incomplete[loc_start]
+
+    # init <current> location and distance
+    loc_curr = loc_start
+    dist_curr = 0
+
+    # helper functions
+    def get_val(loc):
+        return grid[loc[0]][loc[1]]
+
+    def reachable(loc_curr , loc_dst):
+        return get_val(loc_curr) - get_val(loc_dst) >= -1
+
+    def valid_loc(loc):
+        return 0 <= loc[0] < n_rows and 0<=loc[1]< n_cols
+
+    # Computes Manhattan Distance between current node and end node
+    def compute_guess(loc_curr,loc_end):
+        row_end,col_end = loc_end
+        row_curr,col_curr = loc_curr
+        delta_row = row_end - row_curr
+        delta_col = col_end - col_curr
+        return sqrt(delta_row**2 + delta_col**2)
+
+    h = []
+
+    count = 0
+    
+    while loc_curr != loc_end:
+        count += 1
+        incomplete_keys = d_incomplete.keys()
+
+        # update all 4 neighbors of current node
+        for delta in [(-1,0) , (1,0) , (0,-1) , (0,1)]:
+
+            loc_neighbor = (loc_curr[0] + delta[0] , loc_curr[1] + delta[1])
+
+            # check if loc_neighbor is actually on the grid
+            if valid_loc(loc_neighbor):
+
+                # check if loc_neighbor still hasn't been completed
+                if loc_neighbor in incomplete_keys:
+
+                    # check if loc_neighbor can be reached according to rules of AOC 2022/Day 12
+                    if reachable(loc_curr , loc_neighbor):
+
+                        # check if we've found a new shortest path to neighbor                       
+                        if (dist_curr + 1) <= d_incomplete[loc_neighbor][0]:
+                            d_incomplete[loc_neighbor] = (dist_curr+1 , loc_curr)
+                            
+                            # heap is now sorted by min Manhattan Distance
+                            delta_m = compute_guess(loc_curr, loc_end)
+                            heapq.heappush(h , (dist_curr+delta_m, dist_curr+1 , loc_neighbor) )
+
+        # find minimum incomplete node
+        while True:
+            pv = heapq.heappop(h)
+            loc_next = pv[2]
+            min_dist = pv[1]
+            if loc_next not in d_complete.keys():
+                break
+
+        # make min node the new current node
+        dist_curr = min_dist
+        loc_curr = loc_next
+
+        # move new current node from incomplete
+        # to complete dictionary
+        d_complete[loc_next] = d_incomplete[loc_next]
+        del d_incomplete[loc_next]
+
+    print("Num of loops: ", count)
 
     # return shortest path length
     return d_complete[loc_end][0]
@@ -182,7 +289,8 @@ def main():
     grid , loc_start, loc_end = load_grid(fname[opt])
 
     # find the shortest path from loc_start to loc_end
-    n = djk(grid,loc_start,loc_end)
+    #n = djk(grid,loc_start,loc_end)
+    n = AStar(grid, loc_start, loc_end)
 
     # print the results
     print("\nshortest path: " , n)
